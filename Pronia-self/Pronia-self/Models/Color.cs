0e48d2a6d755145f2e985a93098b951521e704d3
AytajMammadova306 <aytajamam@code.edu.az>
2025-12-01 17:40:02 +0400

    initial

---
+++ Pronia-self/Pronia-self/Models/Color.cs
@@ -0,0 +1,8 @@
+﻿namespace Pronia_self.Models
+{
+    public class Color:BaseEntity
+    {
+        public string Name { get; set; }
+        public List<ProductColor> ProductColor { get; set; }
+    }
+}
--- Pronia-self/Pronia-self/obj/Debug/net8.0/Pronia-self.AssemblyInfo.cs
+++ Pronia-self/Pronia-self/obj/Debug/net8.0/Pronia-self.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia-self")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28538285acd6bcd38f1e3ae754745a8af790db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a8822f02a8b9fb970b1b497c24a2d934d0cbed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia-self")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia-self")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
